a73e4bad754f57a72e587d9ebce940eee3a12aad
kushagraaa <gulabarora07@gmail.com>
2018-02-10 17:36:15 +0530

    Added Astar code

--- Astar/Astar.cpp
+++ Astar/Astar.cpp
@@ -16,7 +16,6 @@ typedef double cost_t;
 double perVMpenalty;
 double startupCost;
 
-
 /*
 	query class
 	every query(even same cost) should have unique name
@@ -31,7 +30,7 @@ public:
 	string name;
 	cost_t cost;
 	
-	cost_t min_cost(){
+	cost_t min_cost() const{
 		return cost;
 	}
 	inline bool operator==(const query & other) const{
@@ -131,7 +130,7 @@ size_t hash_value(const query & x)
 }
 
 namespace std {
-	// only accounts for the remaining queries, part of std, therefore can be called by std::hash<vertex>()
+	// accounts for the remaining queries and last_machine_no, part of std, therefore can be called by std::hash<vertex>()
 	template <>
 	struct hash<vertex>
 	{
@@ -155,7 +154,7 @@ namespace std {
 */
 class Graph{
 public:
-	static cost_t heuristic(vertex & a, vertex & b){
+	static cost_t heuristic(const vertex & a, const vertex & b){
 		set<query> m(b.q.begin(),b.q.end());
 		cost_t ans = 0;
 		for(int i = 0; i < a.q.size(); i++){
@@ -164,7 +163,7 @@ public:
 		}
 		return ans;
 	}
-	static void get_neighbours(vertex & a, vector<vertex> & neighbors){
+	static void get_neighbours(const vertex & a, vector<vertex> & neighbors){
 		neighbors.clear();
 		vertex b;
 		if(a.last_machine_time!=0 || a.last_machine_no==0){
@@ -183,7 +182,7 @@ public:
 				}
 			}
 	}
-	static cost_t cost(vertex & a, vertex & b){
+	static cost_t cost(const vertex & a, const vertex & b){
 		set<query> m(b.q.begin(),b.q.end());
 		cost_t ans = 0;
 		for(int i = 0; i < a.q.size(); i++){
@@ -196,60 +195,64 @@ public:
 	}
 };
 
-// /*
-// 	//to do
-// 	check it once with harsha, add comments also
-// */
-// template<typename Graph>
-// void a_star_search
-// 	(Graph G, vertex start, vertex goal,
-// 	function<typename Graph::cost_t(const vertex & a, const vertex & b)> heuristic,
-// 	unordered_map<vertex, vertex, boost::hash<vertex> >& came_from,
-// 	unordered_map<vertex, typename Graph::cost_t, boost::hash<vertex> >& cost_so_far)
-// {
-// 	typedef typename Graph::vertex vertex;
-// 	typedef typename Graph::cost_t cost_t;
+/*
+	Astar set implementation
+*/ 
+void Astar(vertex & start, vertex & goal,
+	unordered_map<vertex, vertex> & came_from,
+	unordered_map<vertex, cost_t> & cost_so_far,
+	cost_t (*heuristic)(const vertex & a, const vertex & b) = &(Graph::heuristic),
+	const bool print_on = false)
+{
+	set<pair<cost_t,vertex> > openList;
+	vector<vertex> neighbours;
 
-// 	unordered_map<vertex, cost_t, boost::hash<vertex> >& priority;
-// 	set<pair<cost_t,vertex> > frontier;
-// 	vector<vertex> neighbors;
+	openList.insert(make_pair(cost_t(0)+heuristic(start,goal), start));
+	came_from[start] = start;
+	cost_so_far[start] = cost_t(0);
+	if(print_on) cout<<"Start is "<<start<<" and Goal is "<<goal<<endl;
 
-// 	frontier.put(start, cost_t(0));
-// 	came_from[start] = start;
-// 	cost_so_far[start] = cost_t(0);
-	
-// 	while (!frontier.empty()) {
-// 		vertex current = frontier.begin()->second;
-// 		frontier.erase(frontier.begin());
-// 		if (current == goal) {
-// 			break;
-// 		}
-// 		G.get_neighbors(current, neighbors);
-// 		for (vertex next : neighbors) {
-// 			cost_t new_cost = cost_so_far[current] + Graph::cost(current, next);
-// 			if(cost_so_far.find(next) == cost_so_far.end()){
-// 				cost_so_far[next] = INF;
-// 				priority[next] = INF;
-// 			}
-// 			if (new_cost < cost_so_far[next]) {
-// 				cost_so_far[next] = new_cost;
-// 				auto & next_priority = priority.find(next);
-// 				frontier.erase(mp(next_priority->second,next));
-// 				next_priority = new_cost + heuristic(next, goal);
-// 				frontier.insert(mp(next_priority->second,next));
-// 				came_from[next] = current;
-// 			}
-// 		}
-// 	}
-// }
+	while (!openList.empty()) {
+		vertex current = openList.begin()->second;
+		openList.erase(openList.begin());
+
+		if(print_on) cout<<current<<": \t";
+		
+		if (current.same_queries(goal)){
+			goal = current;
+			if(print_on) cout<<"reached goal with "<<current<<endl;
+			break;
+		}
+		
+		Graph::get_neighbours(current, neighbours);
+		for (vertex next : neighbours) {
+			
+			if(print_on) cout<<next<<"\t";
+
+			cost_t new_cost = cost_so_far[current] + Graph::cost(current, next);
+			if(cost_so_far.find(next) == cost_so_far.end())
+				cost_so_far[next] = INF;
+			
+			cost_t h = heuristic(next,goal);
+			if (new_cost < cost_so_far[next]) {
+				openList.erase(make_pair(cost_so_far[next] + h, next));
+				cost_so_far[next] = new_cost;
+				came_from[next] = current;
+				openList.insert(make_pair(cost_so_far[next] + h, next));
+			}
+		}
+		if(print_on) cout<<endl;
+	}
+}
 
 
 /*
 	dijkstra set implementation
 */
-void dijkstra(vertex & start, vertex & goal, bool print_on,
+void dijkstra(vertex & start, vertex & goal,
 	unordered_map<vertex, vertex> & came_from,
-	unordered_map<vertex, cost_t> & cost_so_far)
+	unordered_map<vertex, cost_t> & cost_so_far,
+	const bool print_on = false)
 {
 	set<pair<cost_t,vertex> > frontier;
 	vector<vertex> neighbours;
@@ -281,10 +284,10 @@ void dijkstra(vertex & start, vertex & goal, bool print_on,
 				cost_so_far[next] = INF;
 			
 			if (new_cost < cost_so_far[next]) {
-				frontier.erase(make_pair(cost_so_far[next],next));
+				frontier.erase(make_pair(cost_so_far[next], next));
 				cost_so_far[next] = new_cost;
 				came_from[next] = current;
-				frontier.insert(make_pair(cost_so_far[next],next));
+				frontier.insert(make_pair(cost_so_far[next], next));
 			}
 		}
 		if(print_on) cout<<endl;
@@ -297,19 +300,34 @@ void dijkstra(vertex & start, vertex & goal, bool print_on,
 
 int main(){
 	int n;
-	cin>>n;
-	cin>>perVMpenalty;
-	cin>>startupCost;
+	cin>>n>>perVMpenalty>>startupCost;
 	vector<query> q(n);
 	for(int i = 0; i < n; i++)
 		cin>>q[i];
 	vertex st(q), goal;
 	unordered_map<vertex, vertex, std::hash<vertex> > came_from;
 	unordered_map<vertex, cost_t> cost_so_far;
-	dijkstra(st,goal,false,came_from,cost_so_far);
+	//	testing dijkstra
+	cout<<"Dijkstra results:"<<endl<<endl;
+	dijkstra(st,goal,came_from,cost_so_far);
 	vertex u = goal;
 	stringstream ss;
 	stack<string> s;
+	while(u!=st){
+		vertex v = came_from[u];
+		difference(ss,v,u);
+		s.push(ss.str());
+		ss.str("");
+		u = v;
+	}
+	while(!s.empty()){
+		cout<<s.top()<<endl;
+		s.pop();
+	}
+	// testing Astar
+	cout<<"Astar results:"<<endl<<endl;
+	Astar(st,goal,came_from,cost_so_far);
+	u = goal;
 	while(u!=st){
 		vertex v = came_from[u];
 		difference(ss,v,u);
@@ -330,4 +348,7 @@ a 2 b 3 c 4 d 2.5 e 4
 
 3 3 5
 a 2 b 2 c 1
+
+15 10 10
+a 2.34 b 3.1 c 4.15 d 7.1 e 4.25 f 6.42 g 4.15 h 7.145 i 5.1 j 8.14 k 3.14 l 4.15 m 7.15 n 4.12 o 1.3
 */
